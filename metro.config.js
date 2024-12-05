00435011ad5b90b1530bc509a5fe14bdfe5eeae0
Omar Hegazy <omar.yhegazy@gmail.com>
2024-12-04 22:58:07 -0800

    Project Setup

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -6,6 +6,12 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+  return mergeConfig(defaultConfig, {
+    resolver: {
+      assetExts: [...defaultConfig.resolver.assetExts, 'txt', 'csv'],
+    },
+  });
+})();
